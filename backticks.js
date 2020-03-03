7385ab6d20522f6234e2c2db2a2f9ca12cba406c
Sishir Pokhrel <pokhrelsishir@gmail.com>
2020-03-02 23:35:47 -0600

    Practise for features

--- backticks.js
+++ backticks.js
@@ -1,8 +1,9 @@
+// With Backtick
 const name = "Max";
 const age = 29;
 console.log(`My name is ${name} and I am ${age} years old.`);
 
-
+// Without Backtick
 const name1 = "Max";
 const age1 = 29;
 console.log("My name is " + name1 + " and I am " + age1 + " years old.");
