4d59ba9217a09036ac75ce518d88740186c60300
Sishir Pokhrel <pokhrelsishir@gmail.com>
2020-03-05 22:28:28 -0600

    Practise for features

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,20 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
-router.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
-})
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+});
 
-router.post('/product', (req, res, next) => {
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
 
 module.exports = router;
-
